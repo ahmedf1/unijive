5da70b8b8f07d10acf7d981a61310ba6ecde0335
Farhad Ahmed <fa961@nyu.edu>
2018-01-17 02:01:20 -0500

    changed databases titles

--- unijiveEnv/src/webpages/models.py
+++ unijiveEnv/src/webpages/models.py
@@ -13,6 +13,9 @@ class User(models.Model):
     lastName    = models.CharField(max_length = 30)
     password    = models.CharField(max_length = 20)  #ADD VALIDATORS
 
+    def __str__(self):
+        return self.username + ' (' + str(self.userID) + ')'
+
 
 class MessagesInAChat(models.Model):
     class Meta:
@@ -26,11 +29,14 @@ class MessagesInAChat(models.Model):
                     'User',
                      on_delete=models.PROTECT,
                 )
-    message     = models.CharField(max_length = 30)
-    dateTime    = models.DateTimeField(auto_now_add=True)
+    message     = models.CharField(max_length = 100)
+    dateTime    = models.DateTimeField(auto_now_add=True) #FIX TIMEZONE AMBIGUITY
     important   = models.BooleanField(default = False)
     flag        = models.BooleanField(default = False)
     rating      = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return str(self.chatID) + ' | ' + str(self.messageID) + ' : ' + self.message
 
 class ListofChats(models.Model):
     class Meta:
@@ -39,6 +45,9 @@ class ListofChats(models.Model):
     className    = models.CharField(max_length = 30)
     professor    = models.CharField(max_length = 30)
     s_year       = models.CharField(max_length = 3)
+
+    def __str__(self):
+        return self.className + ' (' + str(self.chatID) + ')'
 
 class User_s_Chats(models.Model):
     class Meta:
@@ -54,7 +63,10 @@ class User_s_Chats(models.Model):
                     )
     status         = models.BooleanField(default = False)
     mutedStatus    = models.BooleanField(default = False)
-    username       = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.userID.username + ' | ' + self.chatID.className + ' ' + self.chatID.professor + ' ' + self.chatID.s_year
+    
 
 
 
