7b69aa86f6e85c892740e363d0fedaf2505e58c8
Rohan Kandi1 <RohanKandi1@Rohans-MacBook-Air.local>
2018-01-21 12:39:33 -0500

    Chat_App

--- Chat_App/Chat_App/routing.py
+++
@@ -1,9 +0,0 @@
-# todo: install channels into unijiveEnv.
-from channels import route
-
-def message_handler(message):
-    print(message['text'])
-
-channel_routing = [
-    route("websocket.receive", message_handler)  # todo: Find out what this means
-]
